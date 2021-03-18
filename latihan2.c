e8f538e519d8dc6a52a5a17e912c0eb529d13610
cutnurhidayanti <80440031+cutnurhidayanti@users.noreply.github.com>
2021-03-18 09:14:41 +0700

    Add files via upload

---
+++ latihan2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+//i=jari, j=phi
+ float i = 0.0 ,v = 0.0;
+ float j= (float) 22/7;
+
+
+ printf("inputkan jari-jari bola:");
+ scanf("%f", &i);
+
+ v= (float)4/3*j*i*i*i;
+
+ printf("volume bola : %f",v);
+
+
+
+
+
+
+  return 0;
+}
